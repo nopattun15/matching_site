f17a57d768afc2731c9d24add4f8940ffc660304
takumi.ito <nopattun15@gmail.com>
2021-03-24 17:02:28 +0900

    Update profile page (Add edit & image upload function)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "src/profile_image_upload"
 
 Rails.start()
 Turbolinks.start()
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_062632) do
+ActiveRecord::Schema.define(version: 2021_03_24_073734) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_03_20_062632) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
     t.integer "gender", default: 0, null: false
+    t.text "self_introduction"
+    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
