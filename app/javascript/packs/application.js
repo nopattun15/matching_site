96862fa1b5e09a8b62c165179dba9892affca588
takumi.ito <nopattun15@gmail.com>
2021-04-01 21:58:58 +0900

    Reaction function

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "src/profile_image_upload"
+import "src/profile_image_uploader"
 import "src/swipe"
 
 Rails.start()
